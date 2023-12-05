9f2211516850fae891edcb5fbcd3258668e235a3
Tiago <caniceirotiago@gmail.com>
2023-12-05 17:09:26 +0000

    Class creation

--- src/MusicCollection.java
+++ src/MusicCollection.java
@@ -1,4 +1,18 @@
 package src;
 
-public class MusicCollection {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public abstract class MusicCollection {
+    protected String name;
+    protected ArrayList<Music> musicList;
+    protected LocalDateTime creationDate;
+
+    public MusicCollection(String name, LocalDateTime creationDate) {
+        this.name = name;
+        this.musicList = new ArrayList<>();
+        this.creationDate = LocalDateTime.now();
+    }
+    public abstract void addMusicToCollection();
+    public abstract void removeMusicFromCollection();
 }
