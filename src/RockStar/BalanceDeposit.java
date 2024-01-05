f87fc8b517d697ef6a1f05580e99504e5c286428
Pedro Monteiro <pmdmonteiro1992@gmail.com>
2024-01-05 11:14:56 +0000

    javadoc

--- src/RockStar/BalanceDeposit.java
+++ src/RockStar/BalanceDeposit.java
@@ -4,15 +4,15 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
- *classe que gere os depósitos na conta do cliente
+ * classe que gere os depósitos na conta do cliente
  */
 public class BalanceDeposit implements Serializable {
     private double balanceToAdd;
     private LocalDateTime dateTime;
 
     /**
-     *adicionar dinheiro
-     * @param balanceToAdd
+     * método para adicionar dinheiro à carteira do cliente
+     * @param balanceToAdd valor a adicionar.
      */
     public BalanceDeposit(double balanceToAdd) {
         this.balanceToAdd = balanceToAdd;
